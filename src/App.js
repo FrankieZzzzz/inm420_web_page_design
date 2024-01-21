ef3100c7f11126df1890eb2d12059caff2873947
FrankieZzzz <zhongweini0703@gmail.com>
2024-01-20 22:16:09 -0500

    set up basic containers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.scss'
+import './App.scss';
+import {Header} from './container'
 
 const App = () => {
   return (
-    <div>
-      hello world
+    <div className='App'>
+      <Header />
     </div>
   )
 }
---
+++ src/container/index.js
@@ -0,0 +1,6 @@
+export { default as Header} from './Header/Header';
+export { default as Hero} from './Hero/Hero';
+export { default as Detial} from './Detial/Detial';
+export { default as Price} from './Price/Price';
+export { default as Footer} from './Footer/Footer';
+
